08ab82b0e8e0c1ea427a1cd7cafa0bf5bedcdb12
marikarem <maria.malovski@escola.pr.gov.br>
2024-09-02 14:40:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "SIM!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                afirmacao: " "
             },
             {
                 texto: "NÃO!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao: " "
             }
         ]
     },
@@ -23,54 +23,57 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Michael Jackson",
-                afirmacao: "Se você escolheu essa alternativa, você está correto! Michael Jackson realmente é considerado o rei do pop"
+                afirmacao: " "
             },
             {
                 texto: "Freddie Mercury",
-                afirmacao: "Se você escolheu essa alternativa, você está errado! Freddie Mercury na verdade é onsiderado o rei do rock"
+                afirmacao: " "
             }
         ]
     },
     {
-        enunciado: "Se você escolheu Michael Jackson, você está correto!",
+        enunciado: "Se você escolheu Michael Jackson, você está correto!o Freddie Mercury é o rei do rock. Continuando sobre cantores, você sabe quem é um dos mais famosos do brasil?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Anitta",
+                afirmacao: " "
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Marília Mendonça",
+                afirmacao: " "
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: " Você esta correto nas duas opções, Anitta transita entre pop e funk, e Marília Mendonça no sertanejo. Mudando um pouco agora, quem você acha que é o mais famoso no forró?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Wesley Safadão.",
+                afirmacao: " "
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: " Xand Avião",
+                afirmacao: " "
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: " Bom se você colocou Wesley Safadão você acertou! O que achou das curiosidades dadas sobre o mundo da musica, você gostou? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Sim",
+                afirmacao: " "
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Não",
+                afirmacao: " "
             }
         ]
     },
+    
+
 ];
+
 
 
 let atual = 0;
@@ -105,7 +108,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Então foi essas as pequenas informações sobre o mundo da musica. Muito obrigado!";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
